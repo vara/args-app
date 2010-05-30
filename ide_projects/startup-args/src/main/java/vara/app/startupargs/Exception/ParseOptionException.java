822a41b736191f68d2eee48cb97eb5e1124f0b61
Grzegorz (vara) Warywoda <war29@wp.pl>
2010-05-30 02:54:40 +0200

    Added exception object for situation while parsing arguments

---
+++ ide_projects/startup-args/src/main/java/vara/app/startupargs/Exception/ParseOptionException.java
@@ -0,0 +1,10 @@
+package vara.app.startupargs.Exception;
+
+/**
+ * User: Grzegorz (vara) Warywoda
+ * Date: 2010-05-30
+ * Time: 01:12:28
+ */
+public class ParseOptionException {
+	private static final Logger log = Logger.getLogger(ParseOptionException.class);
+}
