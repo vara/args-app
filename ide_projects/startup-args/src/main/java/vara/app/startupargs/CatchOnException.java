cb73b6cd8d630269cfd2208e6ec5a6b999adb0ff
Grzegorz (vara) Warywoda <war29@wp.pl>
2010-05-30 03:10:21 +0200

    Created object for catcher exceptions while parsing process

---
+++ ide_projects/startup-args/src/main/java/vara/app/startupargs/CatchOnException.java
@@ -0,0 +1,10 @@
+package vara.app.startupargs;
+
+/**
+ * User: Grzegorz (vara) Warywoda
+ * Date: 2010-05-30
+ * Time: 00:07:31
+ */
+public interface CatchOnException {
+	void caughtException(Exception e);
+}
