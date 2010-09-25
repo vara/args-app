c3a07ee3b42a1b5b49d2c13cf418a21373a716d9
Grzegorz (vara) Warywoda <war29@wp.pl>
2010-09-25 03:48:26 +0200

    Updated parameter for files. Added ability to define path wit URI syntax e.q. file://...

--- ide_projects/startup-args/src/main/java/vara/app/startupargs/AbstractFileValueParameter.java
+++ ide_projects/startup-args/src/main/java/vara/app/startupargs/AbstractFileValueParameter.java
@@ -1,9 +1,11 @@
 package vara.app.startupargs;
 
+import org.apache.log4j.Logger;
 import vara.app.startupargs.base.DefaultParameter;
 import vara.app.startupargs.exceptions.ValidationObjectException;
 
 import java.io.File;
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,17 +16,30 @@ import java.util.List;
  */
 public abstract class AbstractFileValueParameter extends DefaultParameter {
 
+	private static final Logger log = Logger.getLogger(AbstractFileValueParameter.class);
+
+	/**
+	 *
+	 */
 	public static enum BEHAVIOUR{
 		CHECK_EXISTS,
 		DO_NOTHING;
 //		CREATE_IF_NOT_EXIST;
 	}
 
+	private BEHAVIOUR behaviour;
+
+	/**
+	 *
+	 * @return
+	 */
 	public BEHAVIOUR getBehaviour() {
 		return behaviour;
 	}
 
-	private BEHAVIOUR behaviour;
+	public void setBehaviour(BEHAVIOUR behaviour) {
+		this.behaviour = behaviour;
+	}
 
 	public AbstractFileValueParameter(String symbol,String shortSymbol){
 		this(symbol,shortSymbol,BEHAVIOUR.DO_NOTHING);
@@ -42,29 +57,82 @@ public abstract class AbstractFileValueParameter extends DefaultParameter {
 			for(String optionValue : optionValues){
 
 			   for (String path : parseOptionValue(optionValue,100)){
-					files.add(createFile(path));
+
+					File file = checkFile(createFile(path));
+					if(file != null){
+						files.add(file);
+					}
 			   }
 			}
 
 			handleOptionFiles(files);
 	}
 
-	protected File createFile(String path){
-		File f = new File(path);
+	/**
+	 *
+	 * @param file
+	 * @return
+	 */
+	protected File checkFile(File file){
 
-		switch (behaviour) {
-			case CHECK_EXISTS:
-				if(!f.exists()){
-					f = null;
-				}
-				break;
-			case DO_NOTHING:	break;
-//			case CREATE_IF_NOT_EXIST:	break;
+		if(file != null){
+			switch (behaviour) {
+				case CHECK_EXISTS:
+					boolean exists = file.exists();
+
+					if(log.isDebugEnabled())log.debug("Detected CHECK_EXISTS mode, exists="+exists);
+
+					if(!exists){
+
+						file = null;
+					}
+					break;
+				case DO_NOTHING:	break;
+	//			case CREATE_IF_NOT_EXIST:	break;
+			}
 		}
-		return f;
+		return file;
 	}
 
-	private String[] parseOptionValue(String value,int limit){
+	/**
+	 *
+	 * @param path
+	 * @return
+	 */
+	private static File createFile(String path){
+
+		if(log.isDebugEnabled())log.debug("Try create File object from path '"+path+"'");
+
+		File file=null;
+		URI uri = createURI(path);
+
+		if(uri != null){
+			try{
+				file = new File(uri);
+				if(log.isDebugEnabled())log.debug(" from URI:"+uri);
+
+			}catch (IllegalArgumentException e){}
+
+		}
+
+		if(file == null){
+			file = new File(path);
+		}
+
+		return file;
+	}
+
+	private static URI createURI(String path){
+
+		try{
+			return new URI(path);
+		}catch (Exception e){}
+
+		return null;
+	}
+
+	private static String[] parseOptionValue(String value,int limit){
+		//TODO: Make separator 
 		String [] paths = value.split(",",limit);
 		return paths;
 	}
--- ide_projects/startup-args/src/test/java/vara/app/startupargs/FileValueParameterTest.java
+++ ide_projects/startup-args/src/test/java/vara/app/startupargs/FileValueParameterTest.java
@@ -1,49 +1,74 @@
 package vara.app.startupargs;
 
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.ConsoleAppender;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PatternLayout;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import vara.app.startupargs.exceptions.ValidationObjectException;
+
+import java.io.File;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * FileValueParameter Tester.
  *
- * @author <Authors name>
+ * @author Grzegorz (vara) Warywoda
  * @since <pre>06/20/2010</pre>
  * @version 1.0
  */
 public class FileValueParameterTest{
+	
+	private static Logger log = Logger.getLogger(FileValueParameterTest.class);
 
+	static final String [] VALID_PATHS = {"file:///home/",
+							  "file:///home",
+							 "//home",
+							  "/home"};
 
-    /**
-     *
-     * Method: isExit()
-     *
-     */
-    public void testIsExit() throws Exception {
-        //TODO: Test goes here...
-    }
+	static final String [] INVALID_PATHS = {"files:///home/",
+							  "file:///homeeee",
+							 "//?home",
+							  "//!/home///"};
+	
+	FileListParameter testedFileListParameter;
+	
+	@BeforeClass
+	public static void setUp(){
+		BasicConfigurator.configure(new ConsoleAppender(new
+			PatternLayout(PatternLayout.DEFAULT_CONVERSION_PATTERN)));
+	}
+	
+	@Before
+	public void beforeTest(){
 
-    /**
-     *
-     * Method: handleOptionFiles(List<File> files)
-     *
-     */
-    public void testHandleOptionFiles() throws Exception {
-        //TODO: Test goes here...
-    }
+	}
 
-    /**
-     *
-     * Method: handleOption(File optionValue)
-     *
-     */
-    public void testHandleOption() throws Exception {
-        //TODO: Test goes here...
-    }
+	@Test
+	public void validationTest(){
+		log.info("**Validation file path test");
+		final AtomicInteger expectedResults = new AtomicInteger(0);
 
-    /**
-     *
-     * Method: getOptionValuesLength()
-     *
-     */
-    public void testGetOptionValuesLength() throws Exception {
-        //TODO: Test goes here...
+		testedFileListParameter = new FileListParameter("test","test"){
+
+			@Override
+			public void handleOption(List<File> optionValue) throws ValidationObjectException {
+				Assert.assertSame(expectedResults.get(),optionValue.size());
+			}
+
+			@Override public String getOptionDescription() {return null;}
+		};
 		
-    }
+		testedFileListParameter.setBehaviour(AbstractFileValueParameter.BEHAVIOUR.CHECK_EXISTS);
+		
+		expectedResults.set(VALID_PATHS.length);
+		testedFileListParameter.safeOption(VALID_PATHS);
+		expectedResults.set(0);
+		testedFileListParameter.safeOption(INVALID_PATHS);
+
+	}
 }
